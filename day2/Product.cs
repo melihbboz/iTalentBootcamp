a73b36575b3f8f86a2a863b7afde399d8d444bff
melih <melih@DESKTOP-DRLN0R6>
2022-07-28 15:13:27 +0300

    Proje dosyası ekle.

---
+++ day2/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2
+{
+    public class Product
+    {
+        private int _id;
+        int Id { get { return _id; } set { _id = value; } }
+
+
+    }
+}
